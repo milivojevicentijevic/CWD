dbbb5c6fdea656c1e0542438ef56de1522357881
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 18:30:41 +0100

    Loops

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,21 +1,42 @@
-// switch - specify many alternatives
-let x = 5;
-switch (x) {
-    case 1:
-        console.log("x is equal to 1");   
-        break;
-    case 2:
-        console.log("x is equal to 2");   
-        break;
-    case 3:
-        console.log("x is equal to 3");   
-        break;
-    case 4:
-        console.log("x is equal to 4");   
-        break;
-    case 5:
-        console.log("x is equal to 5");   
-        break;
-    default:
-        console.log("No match found."); 
-}    // x is equal to 5
+// while loop, do while loop, for loop:
+///// Number 1
+///// Number 2
+///// Number 3
+// for in loop
+///// name is Jovan
+///// age is 34
+///// job is Manager
+
+// while loop
+let first = 1;
+while (first <= 3) {
+    console.log("Number " + first);
+    first++;
+}
+// do while loop
+let second = 1;
+do {
+    console.log("Number " + second);
+    second++;
+} while (second <= 3);
+// for loop
+for (let third = 1; third <= 3; third++) {
+    console.log("Number " + third); 
+}
+// for in loop
+let person = {
+    name: "Jovan",
+    age: "34",
+    job: "Manager"
+};
+for (let x in person) {
+    console.log(x + " is " + person[x]);
+}
+// for of loop
+let cars = new Array('BMW', 'Volvo', 'Audi');
+for (let i in cars) {
+    console.log(cars[i]);
+}    // BMW, Volvo, Audi (one below another )
+for (let i of cars) {
+    console.log(i);
+}    // BMW, Volvo, Audi (one below another )
