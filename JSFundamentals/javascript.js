cfd0def01d634620277666dff1752ad0dae3d6f1
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 15:32:53 +0100

    Type conversion

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,39 +1,43 @@
-/// Primitive data types
-let name = 'Milivoje';
-console.log(name);    // Milivoje
-console.log(typeof name);    // string
-let age = 24;
-console.log(age);    // 24
-console.log(typeof age);    // Number
-let isTrue = true;
-console.log(isTrue);    // true
-console.log(typeof isTrue);    // boolean
-let phone = null;
-console.log(phone);    // null
-console.log(typeof phone);    // object
-let car;
-console.log(car);    // undefined
-console.log(typeof car);    // undefined
-let symbol = Symbol();
-console.log(symbol);    // Symbol()
-console.log(typeof symbol);    // symbol
-/// Reference types (all come back as object)
-const cars = ['Volkswagen','Audi','BMW'];
-console.log(cars);    // (3) ["Volkswagen", "Audi", "BMW"]
-console.log(typeof cars);    // object
-const fruits = new Array('Apple','Pear','Plum');
-console.log(fruits);    // (3) ["Apple", "Pear", "Plum"]
-console.log(typeof fruits);    // object
-var personalInfo = {
-    city: 'Valjevo',
-    country: 'Serbia'
-};
-console.log(personalInfo);    // {city: "Valjevo", country: "Serbia"}
-console.log(typeof fruits);    // object
-const today = new Date();
-console.log(today);
-console.log(typeof today);
-
-
-
-
+let val;
+/// to string
+val = String(2);
+console.log(val);    // 2
+console.log(typeof val);    // string
+console.log(val.length);    // 1
+val = String(true);
+console.log(val);    // true
+console.log(typeof val);    // string
+console.log(val.length);    // 4 
+val = String(new Date());
+console.log(val);    // Sun Jan 31 2021 14:48:16 GMT+0100...
+console.log(typeof val);    // string
+console.log(val.length);    // 66 
+val = String([1, 2, 3, 4, 5, 6])
+console.log(val);    // 1,2,3,4,5,6
+console.log(typeof val);    // string
+console.log(val.length);    // 11
+val = (5).toString();
+console.log(val);    // 5
+console.log(typeof val);    // string
+/// to number
+val = Number('5');
+console.log(val);    // 5
+console.log(typeof val);    // number
+console.log(val.toFixed(2));    // 5.00
+val = Number(false);
+console.log(val);    // 0
+console.log(typeof val);    // number
+val = Number('My name is Milivoje');
+console.log(val);    // NaN //(not a number)
+console.log(typeof val);    // number
+val = Number([1, 2, 3, 4, 5, 6]);
+console.log(val);    // NaN 
+console.log(typeof val);    // number
+val = parseInt('50.50');
+console.log(val);    // 50
+console.log(typeof val);    // number
+console.log(val.toFixed(2));    // 50.00
+val = parseFloat('100.50');
+console.log(val);    // 100.5
+console.log(typeof val);    // number
+console.log(val.toFixed(2));    // 100.50
