7a45adb7e5dd67941d96eb4f5282f1ba52f6a441
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 16:04:46 +0100

    concatination & appending

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,19 +1,18 @@
+const firstName = 'Petar';
+const lastName = 'Markovic';
+const age = 36;
+
 let val;
-val = Math.PI; 
-console.log(val);    // 3.141592653589793
-val = Math.ceil(2.4);
-console.log(val);    // 3
-val = Math.floor(2.8);
-console.log(val);    // 2
-val = Math.round(2.3);   
-console.log(val);    // 2
-val = Math.sqrt(64);
-console.log(val);    // 8
-val = Math.abs(-4);
-console.log(val);    // 4
-val = Math.pow(8, 2);
-console.log(val);    // 64
-val = Math.min(2, 33, 44, 55, 66, -4);
-console.log(val);    // -4
-val = Math.max(2, 33, 44, 55, 66, -4);
-console.log(val);    // 66
+val = firstName + " " + lastName;
+console.log(val);    // Petar Markovic
+
+val = firstName + " " + lastName + " " + age;
+console.log(val);    // Petar Markovic 36
+
+// appending
+val = "John ";
+val += "Doe";
+val += age;
+
+console.log(val);    // John Doe36
+console.log(typeof val);    // string
