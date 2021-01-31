27a4a4ce0ef40f14a3603510d15362177721b842
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 18:01:57 +0100

    switch statement

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,16 +1,21 @@
-let bool = false;
-if (bool) {
-    console.log("This statement is true");
-} else {
-    console.log("This statement is false"); 
-}    // This statement is false
-
-let x = 6;
-let y = 10;
-if (x == 5 && y == 10) {
-    console.log("Both conditins are true!");
-} else if (x == 5 || y == 10) {
-    console.log("At least one condition is true!");
-} else {
-    console.log("None of this conditions is true!");
-}    // At least one condition is true!
+// switch - specify many alternatives
+let x = 5;
+switch (x) {
+    case 1:
+        console.log("x is equal to 1");   
+        break;
+    case 2:
+        console.log("x is equal to 2");   
+        break;
+    case 3:
+        console.log("x is equal to 3");   
+        break;
+    case 4:
+        console.log("x is equal to 4");   
+        break;
+    case 5:
+        console.log("x is equal to 5");   
+        break;
+    default:
+        console.log("No match found."); 
+}    // x is equal to 5
