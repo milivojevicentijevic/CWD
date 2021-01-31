bf0683e82870d9b8e10ecf43f2c1d80d43b07065
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 18:47:36 +0100

    functions

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,42 +1,19 @@
-// while loop, do while loop, for loop:
-///// Number 1
-///// Number 2
-///// Number 3
-// for in loop
-///// name is Jovan
-///// age is 34
-///// job is Manager
+function showMessage() {
+    console.log("This is our first function");
+}
+showMessage();    // This is our first function
+function printOutMyName(firstName) {
+    console.log("My name is " + firstName);
+}
+printOutMyName("Milivoje");    // My name is Milivoje
+function printOutMyNameAndAge(firstName, age = 35) {
+    console.log("My name is " + firstName + " and I'm " + age);
+}
+printOutMyNameAndAge("Milivoje");    // My name is Milivoje and I'm 35
 
-// while loop
-let first = 1;
-while (first <= 3) {
-    console.log("Number " + first);
-    first++;
+// function with a return value
+function addNumbers(num1, num2) {
+    return num1 + num2;
 }
-// do while loop
-let second = 1;
-do {
-    console.log("Number " + second);
-    second++;
-} while (second <= 3);
-// for loop
-for (let third = 1; third <= 3; third++) {
-    console.log("Number " + third); 
-}
-// for in loop
-let person = {
-    name: "Jovan",
-    age: "34",
-    job: "Manager"
-};
-for (let x in person) {
-    console.log(x + " is " + person[x]);
-}
-// for of loop
-let cars = new Array('BMW', 'Volvo', 'Audi');
-for (let i in cars) {
-    console.log(cars[i]);
-}    // BMW, Volvo, Audi (one below another )
-for (let i of cars) {
-    console.log(i);
-}    // BMW, Volvo, Audi (one below another )
+let add = addNumbers(10, 10);
+console.log(add);
