3108933affabb5995d0a4ac3ab110d3e56551494
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 12:11:49 +0100

    variables

---
+++ JSFundamentals/javascript.js
@@ -0,0 +1,42 @@
+// var - let - const
+
+// var
+var name = 'Milivoje';
+console.log(name);
+name = 'Jovan';
+console.log(name);
+
+var job;
+console.log(job);
+job = 'JavaScript Developer';
+console.log(job);
+
+// let
+let newName = 'Jovan';
+console.log(newName);
+
+// const
+const car = 'Mercedes';
+console.log(car);
+// have to assig it to a value
+// const phone;
+// console.log(phone); // error message
+
+// car = 'Audi';
+// console.log(car); // error (read only)
+
+const person = {
+    name: 'Jovan',
+    age: 30
+};
+console.log(person);
+person.name = 'Jovica';
+console.log(person);
+
+
+
+
+
+
+
+
