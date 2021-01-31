742ec7054c918254a6566f33121442dcabf56ec3
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 17:14:40 +0100

    arrays

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,24 +1,32 @@
-let val = 15;
-console.log(val);    // 15
+const jobs = ['Software dev','HR Analyst','Marketing','Finance'];
+console.log(jobs);    // (4) ["Software dev", "HR Analyst", "Marketing", "Finance"]
+const age = new Array(22, 23, 24, 55, 74, 55, 18);
+console.log(age);    // (7) [22, 23, 24, 55, 74, 55, 18]
 
-// += add and assign operator
-val += 5;
-console.log(val);    // 20
+let test;
+test = age.length;
+console.log(test);    // 7
+test = Array.isArray(age);
+console.log(test);    // true
+test = jobs['Marketing'];
+console.log(test);    // undefined
+test = jobs[0];
+console.log(test);    // Software dev
+jobs.push('Data analyst');
+console.log(jobs);    // (5) ["Software dev", "HR Analyst", "Marketing", "Finance", "Data analyst"]
+jobs.pop();
+console.log(jobs);    // (4) ["Software dev", "HR Analyst", "Marketing", "Finance"]
+jobs.shift();
+console.log(jobs);    // (3) ["HR Analyst", "Marketing", "Finance"]
+jobs.unshift('Software dev');
+console.log(jobs);    // 4) ["Software dev", "HR Analyst", "Marketing", "Finance"]
+test = jobs.join(' | ');
+console.log(test);    // Software dev | HR Analyst | Marketing | Finance
+test = jobs.reverse();
+console.log(test);    // (4) ["Finance", "Marketing", "HR Analyst", "Software dev"]
+test = jobs.sort();
+console.log(test);    // (4) ["Finance", "HR Analyst", "Marketing", "Software dev"]
 
-// -= sibtract and assign operator
-val -= 5;
-console.log(val);    // 15
-
-// *= multiply and assign operator
-val *= 5;
-console.log(val);    // 75
-
-// /= divide and assign operator
-val /= 5;
-console.log(val);    // 15
-
-// %= modulus and assign operator
-val %= 5;
-console.log(val);    // 0
-
-
+const mixed = ["word", 23, 16, true, undefined, "TH"];
+test = mixed.sort();
+console.log(test);    // (6) [16, 23, "TH", true, "word", undefined]
