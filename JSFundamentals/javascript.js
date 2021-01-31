49a92f8353c7e47d1f7890e27ece0878fe5166a0
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 16:38:18 +0100

    logical operators

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,28 +1,36 @@
-let x;
-let y;
-x = 20;
-y = 20;
-// == equal
-console.log(x == y);    // true
-// == strict equal
-console.log(x === y);    // true
-x = '20';
-y = 20;
-// == equal
-console.log(x == y);    // true
-// == strict equal
-console.log(x === y);    // false
-// != not equal
-console.log(x != y);    // false
-// != strict not equal
-console.log(x !== y);    // true
-x = 10;
-y = 20;
-// > greater than
-console.log(x > y);    // false
-// >= greater than or equal
-console.log(x >= y);    // false
-// < less than
-console.log(x < y);    // true
-// <= less than or equal
-console.log(x <= y);    // true
+let x = 20;
+let y = 20;
+
+// &&
+if (x > 11 && y < 15) {
+    console.log('True'); 
+} else {
+    console.log('False');   
+}    // False
+
+// ||
+if (x > 11 || y < 15) {
+    console.log('True'); 
+} else {
+    console.log('False');   
+}    // True
+
+x = true;
+y = false;
+
+// !
+if (!x) {
+    console.log('True'); 
+} else {
+    console.log('False');   
+}    // False
+
+if (!y) {
+    console.log('True'); 
+} else {
+    console.log('False');   
+}    // True
+
+// Ternary operator
+let z = x ? 'True' : 'False';
+console.log(z);    // True
