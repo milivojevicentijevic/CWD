fe902a86ed550ddb66ca8b5c9537395b538da763
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 17:56:52 +0100

    if else

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,26 +1,16 @@
-const now = new Date();
-console.log(now);    // Sun Jan 31 2021 17:26:53 GMT+0100...
-console.log(typeof now);    // object
+let bool = false;
+if (bool) {
+    console.log("This statement is true");
+} else {
+    console.log("This statement is false"); 
+}    // This statement is false
 
-let val;
-val = now.toString();
-console.log(typeof val);    // string
-
-let birthday = new Date(1985, 4, 30, 21, 15, 00);    
-console.log(birthday);    // Thu May 30 1985 21:15:00 GMT+0200 (Central European Summer Time)
-birthday = new Date('May 30 1985 21:15:00');
-console.log(birthday);    // Thu May 30 1985 21:15:00 GMT+0200 (Central European Summer Time)
-birthday = new Date('05/30/1985 21:15:00');
-console.log(birthday);    // Thu May 30 1985 21:15:00 GMT+0200 (Central European Summer Time)
-
-let x;
-x = birthday.getFullYear();
-console.log(x);    // 1985
-x = birthday.getMonth();
-console.log(x);    // 4
-x = birthday.getDay();
-console.log(x);    // 4
-x = birthday.getHours();
-console.log(x);    // 4
-
- 
+let x = 6;
+let y = 10;
+if (x == 5 && y == 10) {
+    console.log("Both conditins are true!");
+} else if (x == 5 || y == 10) {
+    console.log("At least one condition is true!");
+} else {
+    console.log("None of this conditions is true!");
+}    // At least one condition is true!
