5d8f1a879c9ef552679c1d47857f003f8913b1a0
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-31 15:41:53 +0100

    arithmetic operators

--- JSFundamentals/javascript.js
+++ JSFundamentals/javascript.js
@@ -1,43 +1,27 @@
+const num1 = 100;
+const num2 = 50;
 let val;
-/// to string
-val = String(2);
-console.log(val);    // 2
-console.log(typeof val);    // string
-console.log(val.length);    // 1
-val = String(true);
-console.log(val);    // true
-console.log(typeof val);    // string
-console.log(val.length);    // 4 
-val = String(new Date());
-console.log(val);    // Sun Jan 31 2021 14:48:16 GMT+0100...
-console.log(typeof val);    // string
-console.log(val.length);    // 66 
-val = String([1, 2, 3, 4, 5, 6])
-console.log(val);    // 1,2,3,4,5,6
-console.log(typeof val);    // string
-console.log(val.length);    // 11
-val = (5).toString();
-console.log(val);    // 5
-console.log(typeof val);    // string
-/// to number
-val = Number('5');
-console.log(val);    // 5
-console.log(typeof val);    // number
-console.log(val.toFixed(2));    // 5.00
-val = Number(false);
-console.log(val);    // 0
-console.log(typeof val);    // number
-val = Number('My name is Milivoje');
-console.log(val);    // NaN //(not a number)
-console.log(typeof val);    // number
-val = Number([1, 2, 3, 4, 5, 6]);
-console.log(val);    // NaN 
-console.log(typeof val);    // number
-val = parseInt('50.50');
+// addition
+val = num1 + num2;    
+console.log(val);    // 150
+// subtraction
+val = num1 - num2; 
 console.log(val);    // 50
-console.log(typeof val);    // number
-console.log(val.toFixed(2));    // 50.00
-val = parseFloat('100.50');
-console.log(val);    // 100.5
-console.log(typeof val);    // number
-console.log(val.toFixed(2));    // 100.50
+// multiply
+val = num1 * num2; 
+console.log(val);    // 5000
+// divide
+val = num1 / num2; 
+console.log(val);    // 2
+// modules
+val = num1 % num2; 
+console.log(val);    // 0
+// increment - decrement
+val = 1;
+val++;
+console.log(val);    // 2
+++val;
+console.log(val);    // 3
+val--;
+console.log(val);    // 2
+
